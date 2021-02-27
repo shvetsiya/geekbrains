f9a5e74cffe45eed4684c96ed41ffb4e65581f94
Alex Shvets <shvetsiya@gmail.com>
2021-02-26 22:38:51 -0800

    added makefile for cros compilation and examples for godoc

--- golang1-04/fibonacci/fibonacci.go
+++ golang1-04/fibonacci/fibonacci.go
@@ -1,35 +1,20 @@
-package main
+package fibonacci
 
-import "fmt"
-
-func main() {
-	memo := make(map[int64]int64)
-
-	var n int64 = 1
-	fmt.Printf("Fibonacci[%d] = %d and (Fib0 == Fib1) is %t\n", n, fibonacci0(n), fibonacci0(n) == fibonacci1(n, memo))
-
-	n = 2
-	fmt.Printf("Fibonacci[%d] = %d and (Fib0 == Fib1) is %t\n", n, fibonacci0(n), fibonacci0(n) == fibonacci1(n, memo))
-
-	n = 20
-	fmt.Printf("Fibonacci[%d] = %d and (Fib0 == Fib1) is %t\n", n, fibonacci0(n), fibonacci0(n) == fibonacci1(n, memo))
-}
-
-// fibonacci0 finds nth Fibonacci number
+// Fibonacci0 finds nth Fibonacci number
 // It uses recursive formula to find the numbers: Fn = F(n-1) + F(n-2)
 //
 // Special cases are:
 // F(0) = 0
 // F(1) = 1
 // for more details see https://en.wikipedia.org/wiki/Fibonacci_number
-func fibonacci0(n int64) int64 {
+func Fibonacci0(n int64) int64 {
 	if n == 0 || n == 1 {
 		return n
 	}
-	return fibonacci0(n-1) + fibonacci0(n-2)
+	return Fibonacci0(n-1) + Fibonacci0(n-2)
 }
 
-// fibonacci1 finds nth Fibonacci number
+// Fibonacci1 finds nth Fibonacci number
 // It uses recursive formula to find the numbers: Fn = F(n-1) + F(n-2)
 // as in fibonacci0, but uses optimization based on memoization i.e
 // if we already saw this number before immediately return it without
@@ -40,7 +25,7 @@ func fibonacci0(n int64) int64 {
 // F(1) = 1
 // if n in memo: return memo[n]
 // for more details see https://en.wikipedia.org/wiki/Fibonacci_number
-func fibonacci1(n int64, memo map[int64]int64) int64 {
+func Fibonacci1(n int64, memo map[int64]int64) int64 {
 	v, isExit := memo[n]
 	if isExit == true {
 		return v
@@ -49,6 +34,6 @@ func fibonacci1(n int64, memo map[int64]int64) int64 {
 	if n == 0 || n == 1 {
 		return n
 	}
-	memo[n] = fibonacci1(n-1, memo) + fibonacci1(n-2, memo)
+	memo[n] = Fibonacci1(n-1, memo) + Fibonacci1(n-2, memo)
 	return memo[n]
 }
--- golang1-04/fibonacci/fibonacci_test.go
+++ golang1-04/fibonacci/fibonacci_test.go
@@ -1,18 +1,23 @@
-package main
+package fibonacci
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Example() {
+	fmt.Println("Blablabla")
+}
 
+func ExampleFibonacci2() {
+	fmt.Println("Fibonacci2")
 }
 
 func TestFibonacci2(t *testing.T) {
 	memo := make(map[int64]int64)
 
 	var n int64 = 2
-	if fibonacci0(n) != 1 && fibonacci1(n, memo) != 1 {
+	if Fibonacci0(n) != 1 && Fibonacci1(n, memo) != 1 {
 		t.Errorf("fibonacci0 is not equal to fibonacci1 at n = %d", n)
 	}
 }
@@ -21,7 +26,7 @@ func TestFibonacci10(t *testing.T) {
 	memo := make(map[int64]int64)
 
 	var n int64 = 10
-	if fibonacci0(n) != 55 && fibonacci1(n, memo) != 55 {
+	if Fibonacci0(n) != 55 && Fibonacci1(n, memo) != 55 {
 		t.Errorf("fibonacci0 is not equal to fibonacci1 at n = %d", n)
 	}
 }
@@ -30,14 +35,14 @@ func TestFibonacci15(t *testing.T) {
 	memo := make(map[int64]int64)
 
 	var n int64 = 20
-	if fibonacci0(n) != fibonacci1(n, memo) {
+	if Fibonacci0(n) != Fibonacci1(n, memo) {
 		t.Errorf("fibonacci0 is not equal to fibonacci1 at n = %d", n)
 	}
 }
 
 func BenchmarkFibonacci0(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		if x55 := fibonacci0(10); x55 != 55 {
+		if x55 := Fibonacci0(10); x55 != 55 {
 			b.Fatalf("Unexpected value : %d", x55)
 		}
 	}
@@ -46,7 +51,7 @@ func BenchmarkFibonacci0(b *testing.B) {
 func BenchmarkFibonacci1(b *testing.B) {
 	memo := make(map[int64]int64)
 	for i := 0; i < b.N; i++ {
-		if x55 := fibonacci1(10, memo); x55 != 55 {
+		if x55 := Fibonacci1(10, memo); x55 != 55 {
 			b.Fatalf("Unexpected value : %d", x55)
 		}
 	}
